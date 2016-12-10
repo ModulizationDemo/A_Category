5f32305b1d9f60e9170143e02261b49ecf49a883
Casa Taloyum <casatwy@msn.com>
2016-12-10 16:00:49 +0800

    1

---
+++ A_Category/A_Category/CTMediator+A.m
@@ -0,0 +1,21 @@
+//
+//  CTMediator+A.m
+//  A_Category
+//
+//  Created by casa on 2016/12/10.
+//  Copyright © 2016年 casa. All rights reserved.
+//
+
+#import "CTMediator+A.h"
+
+@implementation CTMediator (A)
+
+- (UIViewController *)A_aViewController
+{
+    /*
+        AViewController *viewController = [[AViewController alloc] init];
+     */
+    return [self performTarget:@"A" action:@"viewContrller" params:nil shouldCacheTarget:NO];
+}
+
+@end
